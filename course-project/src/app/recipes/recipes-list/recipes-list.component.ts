f1945faf91a331cc1bccbc63f8a24bc345db36bf
Paul <vaneveld.paul@gmail.com>
2019-01-09 16:34:04 +0100

    work in progress

---
+++ course-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.scss']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
